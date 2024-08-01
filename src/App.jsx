324b8876b2e24128b6bb79a34ab964b0bd7ff538
Gary <30745564+CJ20170805@users.noreply.github.com>
2024-08-01 15:42:50 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./mvp.css";
 import "./styles.css";
 import SearchBar from "./components/SearchBar/index";
+import WeatherInfo from "./components/WeatherInfo";
+import { useState } from "react";
 
 export default function App() {
+  const [info, setInfo] = useState([]);
+
+  const setWeatherInfo = (info) => {
+    setInfo(info);
+  };
+
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar setWeatherInfo={setWeatherInfo} />
+      <WeatherInfo info={info} />
     </div>
   );
 }
